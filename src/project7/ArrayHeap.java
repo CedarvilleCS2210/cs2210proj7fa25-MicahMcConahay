41d59b6ed50e26cc4a58dd1630915386de8091eb
Micah McConahay <micahmcconahay@cedarville.edu>
2025-12-02 20:55:38 -0500

    Add removeRoot method to ArrayHeap
    Implemented the removeRoot method to handle root removal and maintain heap properties.

--- src/project7/ArrayHeap.java
+++ src/project7/ArrayHeap.java
@@ -46,13 +46,94 @@ public class ArrayHeap extends ArrayBinaryTree implements Heap {
 
 	  //Postprocessing:
 	  bubbleUp(size - 1);
-      return null;
   }
 
   public Object removeRoot() throws EmptyHeapException {
       // TODO: add code here
-      return null;
+	  // Code has been added
+
+	  // Preprocessing: Check for empty array and save root value
+	  // Processing: Move the last node to the root and remove the last position
+	  // Postprocessing: Decrease size, bubble down
+
+	  //Preprocessing:
+	  if (size == 0) {
+		  throw new EmptyHeapException("Heap is empty");
+	  }
+	  Item rootItem = (Item) btArray[0].element();
+
+	  //Processing:
+	  btArray[0].setElement(btArray[size - 1].element());
+	  btArray[size - 1] = null;
+
+	  //Postprocessing:
+	  size--;
+	  if (size > 0) {
+		  bubbleDown(0);
+	  }
+	  return rootItem;
   }
+	  private void bubbleUp(int index) {
+          int current = index;
+
+          while (current > 0) {
+              int parentIndex = (current - 1) / 2;
+
+              Item currItem = (Item) btArray[current].element();
+              Item parentItem = (Item) btArray[parentIndex].element();
+
+              try {
+                  if (heapComp.isLessThanOrEqualTo(parentItem.key(), currItem.key())) {
+                      return;
+                  }
+              } catch (InvalidObjectException e) {
+                  throw e;
+              }
+
+              swap(current, parentIndex);
+              current = parentIndex;
+          }
+      }
+
+	  private void bubbleDown(int index) {
+          int current = index;
+
+          while (true) {
+              int left  = current * 2 + 1;
+              int right = current * 2 + 2;
+
+              if (left >= size) break;
+
+              int smaller = left;
+
+              if (right < size) {
+                  Item leftItem  = (Item) btArray[left].element();
+                  Item rightItem = (Item) btArray[right].element();
+
+                  try {
+                      if (heapComp.isLessThan(rightItem.key(), leftItem.key())) {
+                          smaller = right;
+                      }
+                  } catch (InvalidObjectException e) {
+                      throw e;
+                  }
+              }
+
+              Item currentItem = (Item) btArray[current].element();
+              Item childItem   = (Item) btArray[smaller].element();
+
+              try {
+                  if (heapComp.isLessThanOrEqualTo(currentItem.key(), childItem.key())) {
+                      break;
+                  }
+              } catch (InvalidObjectException e) {
+                  throw e;
+              }
+
+              swap(current, smaller);
+              current = smaller;
+          }
+      }
 
         // you may want to expand main; it is just provided as a sample
     public static void main (String[] args) {
